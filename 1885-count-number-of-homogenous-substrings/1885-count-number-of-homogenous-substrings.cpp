ba013e3966da945285686dce52d20ae16ed7218a
Ishit Singh <100533099+ishit1011@users.noreply.github.com>
2023-11-09 22:29:05 +0530

    Time: 20 ms (93.7%), Space: 12 MB (85.65%) - LeetHub

--- 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.cpp
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int countHomogenous(string s) {
+        // 1. Initialize total no. of substrings as length of string
         long sum = s.length();
         const int MOD = 1000000007;
+    
         int l = 0, h = 1;
         while(h < s.size()){
+            // 3. When s[low] != s[high] -->  re-shift the sliding window
             if(s[h] != s[l]){
                 l=h;
             }
             else{
+                // 2. When s[low] = s[high] --> sum is increased by no.
+                //                              of elements b/w "l" & "h"
                 sum += (h-l);
             }
             h++;
         }
         return sum % MOD;
+        // As written in the question
     }
 };
