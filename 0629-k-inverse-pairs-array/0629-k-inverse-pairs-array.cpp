e511f8b810a943eb22591ff5e8a29aa765cbe040
Ishit Singh <100533099+ishit1011@users.noreply.github.com>
2024-01-28 03:11:54 +0530

    Time: 2740 ms (6.49%), Space: 11.7 MB (40.00%) - LeetHub

---
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    const int mod = int(1e9+7);
+    int solveMem(int n, int k){
+        if(k <= 0){
+            return k==0;
+        }
+        if(dp[n][k] != -1){
+            return dp[n][k];
+        }
+
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            ans += solveMem(n-1,k-i);
+            ans %= mod;
+        }
+        return dp[n][k] = ans;
+    }
+    int kInversePairs(int n, int k) {
+        memset(dp,-1,sizeof(dp));
+        return solveMem(n,k);
+    }
+};
