cd3fe8ad651a410245e26210adc92a78b27e4e1f
Ishit Singh <100533099+ishit1011@users.noreply.github.com>
2025-04-10 23:54:11 +0530

    Time: 0 ms (100%), Space: 71.7 MB (53.33%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        int max_ending_here = 0;
-        int max_so_far = INT_MIN;
-
+        // Kadanes algorithm
+        int maxSumHere = -10000, maxSubarraySum = -10000;
         for(int i=0; i<n; i++){
-            max_ending_here += nums[i];
-            if(max_ending_here > max_so_far){
-                max_so_far = max_ending_here;
-            }
-            if(max_ending_here < 0){
-                max_ending_here = 0;
-            }
+            int curr = maxSumHere+nums[i];
+            maxSumHere = max(curr,nums[i]);
+            maxSubarraySum = max(maxSumHere,maxSubarraySum);
         }
-        return max_so_far;
+        return maxSubarraySum;
     }
 };
