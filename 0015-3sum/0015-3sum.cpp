b4a72145b28f5ccfb44fe5be49e4c6b00427e24c
Ishit Singh <100533099+ishit1011@users.noreply.github.com>
2025-05-01 23:01:42 +0530

    Time: 47 ms (72.29%), Space: 31.1 MB (26.65%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,6 +5,7 @@ public:
         int n = nums.size();
         sort(nums.begin(),nums.end());
         for(int i=0; i<=n-2; i++){
+            // skip the duplicates
             if(i != 0 && nums[i] == nums[i-1]) continue;
             int l = i+1, r = n-1;
             while(l < r){
