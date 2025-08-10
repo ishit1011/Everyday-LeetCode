e9b163c2bf5c8edc17e1d9f2b97420e6fcfd155a
Ishit Singh <100533099+ishit1011@users.noreply.github.com>
2025-08-11 00:01:12 +0530

    Time: 0 ms (100%), Space: 8 MB (17.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) return false;
+         if(n <= 0) return false;
         return ((n & (n-1)) == 0);
     }
 };
-
-/*
-    Now consider :-
-    bit representation of 7  -> 0111
-    bit representation of 8  -> 1000
-    bitwise AND of 7 and 8 -> 0000
-    we are gonna use this property for for all numbers which are powers of two
-*/
