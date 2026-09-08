27eb72303bd61388327fab175e37adac92bbdf97
Ishit Singh <100533099+ishit1011@users.noreply.github.com>
2026-09-09 01:22:01 +0530

    Time: 7 ms (10.03%), Space: 8.6 MB (15.44%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int cnt = 0;
+        int m = n;
+        while(n > 0){
+            n /= 10;
+            cnt++;
+        }
+        
+        if(m > 999){
+            int commas = cnt / 3, nums = m - 999;
+            cout<<"commas : "<<commas<<"\nnums : "<<(m-999)<<"\nmult : "<<(cnt/3) * (m-999);
+            if(cnt % 3 == 0) return (commas-1) * nums;
+            else return commas * nums;
+        }
+        return 0;
+    }
+};
